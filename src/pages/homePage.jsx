13bee51dcc2823c53f4696938e4b8d8a9cac57f7
Mauritz Marais <mauritz303@gmail.com>
2023-06-20 16:15:42 +0200

    created home page

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/header"
+import Sidebar from "../components/sidebar"
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Sidebar />
+
+            </main>
+        </>
+    )
+};
