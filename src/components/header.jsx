1f4b795a389748d48cf05dcacb7cbf2449486a81
Mauritz Marais <mauritz303@gmail.com>
2023-06-20 16:13:10 +0200

    created header component

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+    return (
+        <>
+            <header>
+                <h1>Home Page</h1>
+                <p>Welcome to my blog.</p>
+            </header>
+        </>
+    )
+};
