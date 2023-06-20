9ba79c005431fea64374ee143940d0babcf6b375
Mauritz Marais <mauritz303@gmail.com>
2023-06-20 17:53:45 +0200

    added function to get post by id and display it with comments

--- src/pages/postPage.jsx
+++ src/pages/postPage.jsx
@@ -1,13 +1,45 @@
 import Sidebar from "../components/sidebar"
+import CommentsContainer from "../components/comments"
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function Post() {
+    const { id } = useParams();
+    const [post, setPost] = useState({});
+
+    // fetch post by id
+    useEffect(() => {
+        async function getPost() {
+            try {
+                const response = await fetch(`http://localhost:3001/posts/${id}`, {
+                    method: 'GET',
+                });
+
+                const post = await response.json();
+
+                setPost({ ...post });
+            }
+            catch (error) {
+                console.log(error)
+            }
+        };
+
+        getPost();
+    }, []);
+
+
     return (
         <>
+            <header>
+                <Link to="/">Home</Link>
+            </header>
             <main>
                 <article>
-                    <h1>Post title</h1>
+                    <h1>{post.title}</h1>
 
-                    <p>post content....</p>
+                    {/* format body correctly */}
+                    <p>{post.body}</p>
+                    <CommentsContainer postComments={post.comments} />
                 </article>
                 <Sidebar />
             </main>
