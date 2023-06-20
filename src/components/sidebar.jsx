c1a2cc643b2fd3ce70a42303704f98649ef8eaa0
Mauritz Marais <mauritz303@gmail.com>
2023-06-20 17:50:18 +0200

    renamed component, updated basic layout

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
 
 export default function Sidebar() {
+    // get 5 most recent posts*
     return (
         <>
             <aside>
-                <h2>Recent articles</h2>
+                <h3>Recent articles</h3>
+                <p>(display recent articles)</p>
 
                 <Link to="/posts">Archives</Link>
             </aside>
