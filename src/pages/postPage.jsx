5238e372f720b53044136c1ec8460a9fd929757e
Mauritz Marais <mauritz303@gmail.com>
2023-06-20 16:16:20 +0200

    created page to view a post and with comments

---
+++ src/pages/postPage.jsx
@@ -0,0 +1,16 @@
+import Sidebar from "../components/sidebar"
+
+export default function Post() {
+    return (
+        <>
+            <main>
+                <article>
+                    <h1>Post title</h1>
+
+                    <p>post content....</p>
+                </article>
+                <Sidebar />
+            </main>
+        </>
+    )
+};
