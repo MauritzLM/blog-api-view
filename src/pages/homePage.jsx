c97ebcc78d50625f2d8c50d0cf3697c3657c54c0
Mauritz Marais <mauritz303@gmail.com>
2023-06-20 17:52:40 +0200

    imported header and sidebar

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,10 +2,17 @@ import Header from "../components/header"
 import Sidebar from "../components/sidebar"
 
 export default function Home() {
+    // get most recent post*
     return (
         <>
             <Header />
             <main>
+                {/* display most recent post */}
+                <div>
+                    <h2>most recent article</h2>
+                    <p>(first paragraph)</p>
+                    <p>(link to view the rest of the post)</p>
+                </div>
                 <Sidebar />
 
             </main>
