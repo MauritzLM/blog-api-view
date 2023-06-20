1c761b03d866c7fce07a1486c6eb3d723854c286
Mauritz Marais <mauritz303@gmail.com>
2023-06-20 16:17:20 +0200

    added routes for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/homePage';
+import Error from './pages/errorPage';
+import Post from './pages/postPage';
+import AllPosts from './pages/allPostsPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Blog view</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/posts'>
+        <Route index element={<AllPosts />} />
+        <Route path=':id' element={<Post />} />
+
+      </Route>
+      <Route path='*' element={<Error />} />
+    </Routes>
   );
 }
 
