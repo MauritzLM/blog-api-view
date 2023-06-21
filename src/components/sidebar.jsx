ae1ac43adfb01f28a69c3777c83a16a56b54b8a1
Mauritz Marais <mauritz303@gmail.com>
2023-06-21 18:07:33 +0200

    added list to display recent posts

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom"
 
-export default function Sidebar() {
-    // get 5 most recent posts*
+import { Link } from "react-router-dom";
+
+export default function Sidebar({ recenPosts }) {
+
+    // create list item for each recent post*
+    const postList = recenPosts?.map((post, index) => {
+        return <li key={post._id + "-" + index}><Link to={`/posts/${post._id}`}>{post.title}</Link></li>
+    })
+
     return (
         <>
             <aside>
                 <h3>Recent articles</h3>
-                <p>(display recent articles)</p>
+                <ul>{postList}</ul>
 
                 <Link to="/posts">Archives</Link>
             </aside>
