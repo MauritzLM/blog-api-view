72634cb75eeba50620769d1616648f69a59cc386
Mauritz Marais <mauritz303@gmail.com>
2023-06-22 15:32:51 +0200

    updated new comment form state and date formatting

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom"
 import { useState } from "react";
 import parse from "html-react-parser";
+import dayjs from "dayjs";
+const relativeTime = require('dayjs/plugin/relativeTime');
+dayjs.extend(relativeTime);
 
 export default function CommentsContainer({ postComments, commentAdded, setCommentAdded }) {
     const { id } = useParams();
@@ -20,12 +23,15 @@ export default function CommentsContainer({ postComments, commentAdded, setComme
 
 // add new comment form
 function CommentForm({ id, commentAdded, setCommentAdded }) {
+
+    // how to rerender component when post gets updated? (error messages)*
     // form values
-    const [commentAuthor, setCommentAuthor] = useState('');
-    const [commentBody, setCommentBody] = useState('');
+    const initialFormValues = { author: "", body: "", question: "" };
+    const [commentFormValues, setCommentFormValues] = useState({ ...initialFormValues });
+
     // form error messages
-    const [authorError, setAuthorError] = useState('');
-    const [bodyError, setBodyError] = useState('');
+    const initialErrorValues = { author: "", body: "", question: "" };
+    const [errorValues, setErrorValues] = useState({ ...initialErrorValues })
 
     // handle new comment post*
     async function handleNewComment(event) {
@@ -50,10 +56,13 @@ function CommentForm({ id, commentAdded, setCommentAdded }) {
             if (data.errors) {
                 data.errors.forEach(err => {
                     if (err.path === "commentAuthor") {
-                        setAuthorError(err.msg);
+                        setErrorValues({ ...errorValues, author: err.msg });
                     }
                     if (err.path === "commentBody") {
-                        setBodyError(err.msg);
+                        setErrorValues({ ...errorValues, body: err.msg });
+                    }
+                    if (err.path === "securityQuestion") {
+                        setErrorValues({ ...errorValues, question: err.msg });
                     }
                 });
                 return;
@@ -61,6 +70,8 @@ function CommentForm({ id, commentAdded, setCommentAdded }) {
 
             // success reload page?
             console.log(data);
+            setErrorValues({ ...initialErrorValues });
+            setCommentFormValues({ ...initialFormValues });
             setCommentAdded(commentAdded + 1);
         }
         catch (error) {
@@ -73,13 +84,18 @@ function CommentForm({ id, commentAdded, setCommentAdded }) {
                 <h3>Add new Comment</h3>
 
                 <label htmlFor="commentAuthor">enter your name
-                    <input type="text" name="commentAuthor" value={commentAuthor} onChange={(e) => setCommentAuthor(e.target.value)}></input>
-                    <span>{authorError}</span>
+                    <input type="text" name="commentAuthor" value={commentFormValues.author} onChange={(e) => setCommentFormValues({ ...commentFormValues, author: e.target.value })}></input>
+                    <span>{errorValues.author}</span>
                 </label>
 
                 <label htmlFor="commentBody">Your comment
-                    <textarea name="commentBody" value={commentBody} onChange={(e) => setCommentBody(e.target.value)} />
-                    <span>{bodyError}</span>
+                    <textarea name="commentBody" value={commentFormValues.body} onChange={(e) => setCommentFormValues({ ...commentFormValues, body: e.target.value })} />
+                    <span>{errorValues.body}</span>
+                </label>
+
+                <label htmlFor="securityQuestion">What is 13 - 8 ? (security question)
+                    <input type="text" name="securityQuestion" value={commentFormValues.question} onChange={(e) => setCommentFormValues({ ...commentFormValues, question: e.target.value })} />
+                    <span>{errorValues.question}</span>
                 </label>
 
                 <button>submit</button>
@@ -94,6 +110,7 @@ function CommentCard({ comment }) {
         <div className="comment-card">
             {parse(`<p>${comment.body}</p>`)}
             <p>- {comment.author}</p>
+            <p>{dayjs(comment.timestamp).fromNow()}</p>
         </div>
     )
 };
