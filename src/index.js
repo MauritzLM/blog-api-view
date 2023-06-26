acf4a5bc546deadc539b47ba30ac907157fbfdbd
Mauritz Marais <mauritz303@gmail.com>
2023-06-26 17:04:01 +0200

    installed mock service worker, configured code test coverage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,6 +13,9 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
