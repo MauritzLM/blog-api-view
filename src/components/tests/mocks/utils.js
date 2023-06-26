62ce3e61ed918f426183209cf5fdd12fa2e74c31
Mauritz Marais <mauritz303@gmail.com>
2023-06-26 16:58:37 +0200

    added mock posts array

---
+++ src/components/tests/mocks/utils.js
@@ -0,0 +1,27 @@
+
+// mock array of posts
+export const mockPosts = [{
+    _id: 5,
+    title: "Blog Post 1",
+    author: "Gandalf",
+    body: "I write to you on fith day of the year 3434. \n It has come to my attention...",
+    published: true,
+    comments: [],
+    date: "2023-06-07T14:22:58.888+00:00"
+}, {
+    _id: 10,
+    title: "Blog Post 2",
+    author: "Elrond",
+    body: "Stangers from distant lands, friends. \n You have been called here...",
+    published: true,
+    comments: [],
+    date: "2023-06-08T14:25:58.888+00:00"
+}, {
+    _id: 15,
+    title: "Blog Post 3",
+    author: "Frodo",
+    body: "I will take it \n Though I do not know the way",
+    published: true,
+    comments: [],
+    date: "2023-06-08T14:25:58.888+00:00"
+}];
