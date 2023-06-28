171c1b159cbdaafa7f25017bfaf9821327275c32
Mauritz Marais <mauritz303@gmail.com>
2023-06-28 14:32:12 +0200

    added test for input value change

---
+++ src/components/tests/commentForm.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import CommentForm from "../commentForm";
+
+//  test form inputs (values)
+//  form errors display
+//  form post request calls function
+const setCommentAdded = jest.fn();
+
+describe("form functionality", () => {
+
+    it("form input values", async () => {
+        render(<CommentForm id={5} commentAdded={0} setCommentAdded={setCommentAdded} />);
+
+        const authorInput = screen.getByTestId("author");
+        const bodyInput = screen.getByTestId("body");
+        const questionInput = screen.getByTestId("question");
+
+        fireEvent.change(authorInput, { target: { value: "mo" } });
+        fireEvent.change(bodyInput, { target: { value: "This is my comment" } });
+        fireEvent.change(questionInput, { target: { value: "4" } });
+
+        expect(authorInput).toHaveValue("mo");
+        expect(bodyInput).toHaveValue("This is my comment");
+        expect(questionInput).toHaveValue("4");
+    });
+
+    // it("submitting form", () => {
+    //     // const handleNewComment = jest.fn();
+    //     render(<CommentForm id={5} commentAdded={0} setCommentAdded={setCommentAdded} />);
+
+    //     const button = screen.getByTestId("submit");
+
+    //     fireEvent.click(button);
+
+    //     // expect(handleNewComment).toHaveBeenCalled();
+    //     expect(setCommentAdded).toHaveBeenCalled();
+    // });
+});
