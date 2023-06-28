f6e10dc7ae25b5a33242a0100087cd4644aa1b3b
Mauritz Marais <mauritz303@gmail.com>
2023-06-28 10:22:00 +0200

    moved comment card to its own file

---
+++ src/components/commentCard.jsx
@@ -0,0 +1,15 @@
+import parse from "html-react-parser";
+import dayjs from "dayjs";
+const relativeTime = require('dayjs/plugin/relativeTime');
+dayjs.extend(relativeTime);
+
+// card to display a comment
+export default function CommentCard({ comment }) {
+    return (
+        <div className="comment-card">
+            {parse(`<p>${comment.body}</p>`)}
+            <p>- {comment.author}</p>
+            <p>{dayjs(comment.timestamp).fromNow()}</p>
+        </div>
+    )
+};
