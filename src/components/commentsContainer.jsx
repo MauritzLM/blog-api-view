5640d8d1b9f162686f330b327029faccda4ee079
Mauritz Marais <mauritz303@gmail.com>
2023-06-30 16:50:24 +0200

    added show form state and functions to props and conditional rendering of form

--- src/components/commentsContainer.jsx
+++ src/components/commentsContainer.jsx
@@ -2,24 +2,38 @@ import { useParams } from "react-router-dom"
 import dayjs from "dayjs";
 import CommentForm from "./commentForm";
 import CommentCard from "./commentCard";
+import { useState } from "react";
 const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 
 
-export default function CommentsContainer({ postComments, commentAdded, setCommentAdded }) {
+export default function CommentsContainer({ postComments, commentAdded, setCommentAdded, showForm, handleHideForm, handleShowForm }) {
     const { id } = useParams();
 
-    // add state to hide form
-
-    return (
-        <>
-            <section className="comments-section">
-                <h3>Comments</h3>
-                <div className="comments-container">{postComments?.map(comment => {
-                    return <CommentCard comment={comment} key={comment._id} />
-                })}</div>
-                <CommentForm id={id} commentAdded={commentAdded} setCommentAdded={setCommentAdded} />
-            </section>
-        </>
-    )
+    if (showForm) {
+        return (
+            <>
+                <section className="comments-section">
+                    <h3>Comments</h3>
+                    <div className="comments-container">{postComments?.map(comment => {
+                        return <CommentCard comment={comment} key={comment._id} />
+                    })}</div>
+                    <button className="hide-commentform" onClick={() => handleHideForm()}>Hide form</button>
+                    <CommentForm id={id} commentAdded={commentAdded} setCommentAdded={setCommentAdded} />
+                </section>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <section className="comments-section">
+                    <h3>Comments</h3>
+                    <div className="comments-container">{postComments?.map(comment => {
+                        return <CommentCard comment={comment} key={comment._id} />
+                    })}</div>
+                    <button className="show-commentform" onClick={() => handleShowForm()}>Post a comment</button>
+                </section>
+            </>
+        )
+    }
 };
