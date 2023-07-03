32c8a0f759641f1f36d7c776d1e720ba371cef68
Mauritz Marais <mauritz303@gmail.com>
2023-07-03 15:54:03 +0200

    added tests for conditional rendering of form and calling of show/hide form functions on button click

--- src/components/tests/commentsContainer.test.js
+++ src/components/tests/commentsContainer.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CommentsContainer from "../commentsContainer";
 
 // setup
@@ -17,12 +17,40 @@ describe("rendering of comments container component", () => {
         expect(commentList).toHaveLength(postComments.length);
     });
 
-    it("renders new comment form", () => {
-        render(<CommentsContainer postComments={postComments} commentAdded={0} setCommentAdded={setCommentAdded} />);
+    it("renders new comment form when showform prop is true", () => {
+        render(<CommentsContainer postComments={postComments} commentAdded={0} setCommentAdded={setCommentAdded} showForm={true} />);
 
         const commentForm = screen.getByTestId("commentForm");
 
         expect(commentForm).toBeInTheDocument();
+    });
+
+    it("does not render form when showform prop is false", () => {
+        render(<CommentsContainer postComments={postComments} commentAdded={0} setCommentAdded={setCommentAdded} showForm={false} />);
+
+        expect(screen.queryByTestId("commentForm")).not.toBeInTheDocument();
+    });
+
+    it("clicking on show form button calls function", async () => {
+        const handleShowForm = jest.fn();
+        render(<CommentsContainer postComments={postComments} commentAdded={0} setCommentAdded={setCommentAdded} showForm={false} handleShowForm={handleShowForm} />);
+
+        const showFormButton = screen.getByTestId("showform");
+
+        fireEvent.click(showFormButton);
+
+        expect(handleShowForm).toHaveBeenCalled();
+    });
+
+    it("clicking on hide form button calls function", () => {
+        const handleHideForm = jest.fn();
+        render(<CommentsContainer postComments={postComments} commentAdded={0} setCommentAdded={setCommentAdded} showForm={true} handleHideForm={handleHideForm} />);
+
+        const hideFormButton = screen.getByTestId("hideform");
+
+        fireEvent.click(hideFormButton);
+
+        expect(handleHideForm).toHaveBeenCalled();
     });
 });
 
