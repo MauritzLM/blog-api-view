12583c015a2140df8d698c96ec64850d65d7db92
Mauritz Marais <mauritz303@gmail.com>
2023-08-31 10:43:37 +0200

    added conditional rendering for fetching posts

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,35 +4,47 @@ import Sidebar from "../components/sidebar";
 
 export default function Home({ recentPosts }) {
 
-    // get most recent post
-    const mostRecentPost = recentPosts[0];
+    if (recentPosts.length) {
+        // get most recent post
+        const mostRecentPost = recentPosts[0];
 
-    // get 2nd most recent post
-    const secondMostRecentPost = recentPosts[1];
-
-    return (
-        <>
-            <Header />
-            <main>
-
-                <div>
-                    <div>
-                        <h2>{mostRecentPost.title}</h2>
-                        <p>by <span className="author">{mostRecentPost.author}</span></p>
-                        <p>{mostRecentPost.body.split("\n")[0]}</p>
-                        <Link to={`/posts/${mostRecentPost._id}`}>Continue reading</Link>
-                    </div>
+        // get 2nd most recent post
+        const secondMostRecentPost = recentPosts[1];
+        return (
+            <>
+                <Header />
+                <main>
 
                     <div>
-                        <h2>{secondMostRecentPost.title}</h2>
-                        <p>by <span className="author">{mostRecentPost.author}</span></p>
-                        <p>{secondMostRecentPost.body.split("\n")[0]}</p>
-                        <Link to={`/posts/${secondMostRecentPost._id}`}>Continue reading</Link>
-                    </div>
-                </div>
-                <Sidebar recentPosts={recentPosts} />
+                        <div>
+                            <h2>{mostRecentPost.title}</h2>
+                            <p>by <span className="author">{mostRecentPost.author}</span></p>
+                            <p>{mostRecentPost.body.split("\n")[0]}</p>
+                            <Link to={`/posts/${mostRecentPost._id}`}>Continue reading</Link>
+                        </div>
 
-            </main>
-        </>
-    )
+                        <div>
+                            <h2>{secondMostRecentPost.title}</h2>
+                            <p>by <span className="author">{mostRecentPost.author}</span></p>
+                            <p>{secondMostRecentPost.body.split("\n")[0]}</p>
+                            <Link to={`/posts/${secondMostRecentPost._id}`}>Continue reading</Link>
+                        </div>
+                    </div>
+                    <Sidebar recentPosts={recentPosts} />
+
+                </main>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <Header />
+
+                <main>
+                    <h1>Fetching Posts ...</h1>
+                </main>
+
+            </>
+        )
+    }
 };
